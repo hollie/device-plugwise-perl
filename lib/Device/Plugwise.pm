7179cbd465318523bcd7bfd1ea6247222aac1764
Lieven Hollevoet <hollie@lika.be>
2012-09-13 08:05:32 +0200

    Build results of abc3d11 (on master)

---
+++ lib/Device/Plugwise.pm
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+package Device::Plugwise;
+{
+  $Device::Plugwise::VERSION = '1.122570';
+}
+
+# ABSTRACT: First attempt at writing a modern Plugwise Perl module
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Device::Plugwise - First attempt at writing a modern Plugwise Perl module
+
+=head1 VERSION
+
+version 1.122570
+
+=head1 AUTHOR
+
+Lieven Hollevoet <hollie@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Lieven Hollevoet.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
